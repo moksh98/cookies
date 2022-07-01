ebc74f4a2203d4ff9073da6105c22effd9c6bfad
mrjain <mrjain@ncsu.edu>
2022-07-01 02:08:56 -0400

    inserting and fetching user credentials from database

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,27 @@
 from flask import Flask, render_template, make_response, request, redirect
 from hashlib import sha256
+import psycopg2
+
 
 app = Flask(__name__)
 
+def getConnection(host, port, database, user, password):
+    connection = psycopg2.connect(
+        host=host,
+        port=port,
+        database=database,
+        user=user,
+        password=password)
+    return connection
+
+def getConnection():
+    connection = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="user_data",
+        user="postgres",
+        password="kclds123")
+    return connection
 
 def getHash(string):
     hash = sha256()
@@ -29,8 +48,19 @@ def get_login():
     email = request.form['email']
     password = request.form['psw']
     password_hash = getHash(password)
-    # print(f"Email: {email}, Password: {password_hash}")
-    return redirect('index.html')
+    connection = getConnection()
+    cursor = connection.cursor()
+    query = """ SELECT password from user_credentials WHERE email='""" + email + """';"""
+    cursor.execute(query)
+    database_password = cursor.fetchone()[0]
+    if password_hash == database_password:
+        print("True")
+    else:
+        print("False")
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return redirect('/')
 
 @app.route("/get_signup", methods=["POST"])
 def get_signup():
@@ -42,8 +72,14 @@ def get_signup():
     password_hash = getHash(password)
     password_repeat_hash = getHash(password_repeat)
     if password_hash == password_repeat_hash:
-        pass
-        # print(f"First Name: {fname}, Last Name: {lname}, Email: {email}, Password: {password_hash}, Repeat Password: {password_repeat_hash}")
+        connection = getConnection()
+        cursor = connection.cursor()
+        query = """ INSERT INTO user_credentials (fname, lname, email, password) VALUES (%s,%s,%s,%s)"""
+        data = (fname, lname, email, password_hash)
+        cursor.execute(query, data)
+        connection.commit()
+        cursor.close()
+        connection.close()
     return redirect('/')
 
 if __name__ == '__main__':
