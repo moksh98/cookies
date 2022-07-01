f41ce439a80d4321a868dc5f53220d1d94468e93
mrjain <mrjain@ncsu.edu>
2022-06-30 23:31:31 -0400

    updating the index page

--- flask_app.py
+++ flask_app.py
@@ -10,5 +10,13 @@ def home():
     response.headers["Set-Cookie"] = "myfirstcookie="+str(arr)
     return response
 
+@app.route("/login/")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup/")
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
    app.run()
